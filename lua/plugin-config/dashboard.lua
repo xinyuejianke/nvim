e04dfaa3f8bde06477466d740c2420a223207045
zen <zen_mao@163.com>
2022-05-23 20:44:53 +0800

    edit dashboard

--- lua/plugin-config/dashboard.lua
+++ lua/plugin-config/dashboard.lua
@@ -5,17 +5,18 @@ vim.g.dashboard_custom_section = {
   b = { description = { "  Recently files        " }, command = "Telescope oldfiles" },
   c = { description = { "  Edit keybindings      " }, command = "edit ~/.config/nvim/lua/keybindings.lua" },
   d = { description = { "  Edit Projects         " }, command = "edit ~/.local/share/nvim/project_nvim/project_history", },
-  -- e = { description = { "  Edit .bashrc          " }, command = "edit ~/.bashrc" },
-  f = { description = { "  Edit init.lua         " }, command = "edit ~/.config/nvim/init.lua" },
-  g = { description = {'  Find file          '}, command = 'Telescope find_files'},
-  h = { description = {'  Find text          '}, command = 'Telescope live_grep'},
+  e = { description = { "  Edit init.lua         " }, command = "edit ~/.config/nvim/init.lua" },
+  f = { description = {'  Find file          '}, command = 'Telescope find_files'},
+  g = { description = {'  Find text          '}, command = 'Telescope live_grep'},
 }
 
 vim.g.dashboard_custom_header = {
-  [[ ██╗  ██╗███████╗██╗     ██╗      ██████╗         ███████╗███████╗███╗   ██╗ ]],
-  [[ ██║  ██║██╔════╝██║     ██║     ██╔═══██╗        ╚══███╔╝██╔════╝████╗  ██║ ]],
-  [[ ███████║█████╗  ██║     ██║     ██║   ██║          ███╔╝ █████╗  ██╔██╗ ██║ ]],
-  [[ ██╔══██║██╔══╝  ██║     ██║     ██║   ██║         ███╔╝  ██╔══╝  ██║╚██╗██║ ]],
-  [[ ██║  ██║███████╗███████╗███████╗╚██████╔╝███████╗███████╗███████╗██║ ╚████║ ]],
-  [[ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ ╚══════╝╚══════╝╚══════╝╚═╝  ╚═══╝ ]],
-}                                                                             
+  [[ ███████╗███████╗███╗   ██╗██████╗ ███████╗ ]],
+  [[ ╚══███╔╝██╔════╝████╗  ██║██╔══██╗██╔════╝ ]],
+  [[   ███╔╝ █████╗  ██╔██╗ ██║██║  ██║█████╗   ]],
+  [[  ███╔╝  ██╔══╝  ██║╚██╗██║██║  ██║██╔══╝   ]],
+  [[ ███████╗███████╗██║ ╚████║██████╔╝███████╗ ]],
+  [[ ╚══════╝╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝ ]]
+}
+
+vim.g.dashboard_custom_footer = { "Zen's development environment" }
