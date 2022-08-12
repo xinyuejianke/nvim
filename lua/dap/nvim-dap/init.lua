3ca1ec52506998cf84dfb610967c692dffed05a9
zen <zen_mao@163.com>
2022-08-12 17:09:39 +0800

    remove redundant code

--- lua/dap/nvim-dap/init.lua
+++ lua/dap/nvim-dap/init.lua
@@ -1,10 +1,5 @@
 local dap = require("dap")
 
--- setup nvim-dap-virtual-text
-require("nvim-dap-virtual-text").setup({
-  commented = true,
-})
-
 vim.fn.sign_define("DapBreakpoint", {
   text = "🔴",
   texthl = "LspDiagnosticsSignError",
@@ -26,47 +21,15 @@ vim.fn.sign_define("DapBreakpointRejected", {
   numhl = "",
 })
 
--- -- setup nvim-dap for nodejs adapter
--- require("dap-vscode-js").setup({
---   adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
--- })
---
--- for _, language in ipairs({ "typescript", "javascript" }) do
---   require("dap").configurations[language] = {
---     {
---       name = "Launch file",
---       type = "node2",
---       request = "launch",
---       sourceMaps = true,
---       protocol = 'inspector',
---       skipFiles = {'<node_internals>/**/*.js'},
---       console = 'integratedTerminal',
---       port = 3001,
---       runtimeArgs = {"--inspect=3001"},
---       program = "${workspaceFolder}/app.js",
---       cwd = "${workspaceFolder}"
---     },
---     {
---       name = "Attach",
---       type = "node2",
---       request = "attach",
---       cwd = vim.fn.getcwd(),
---       processId = require 'dap.utils'.pick_process,
---     }
---   }
--- end
-
--- node2 adapter for javascript
--- dap.adapters.node2 = {
---   type = 'executable',
---   command = 'node',
---   args = { os.getenv('HOME') .. "/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js" },
---   -- args = { os.getenv('HOME') .. "/.local/share/nvim/site/pack/packer/opt/vscode-js-debug/out/src/vsDebugServer.js" },
--- }
+-- setup nvim-dap-virtual-text
+require("nvim-dap-virtual-text").setup({
+  commented = true,
+})
 
 local nodejs = require('dap.nvim-dap.dap-nodejs')
 -- setup configuration
 nodejs.setUpDapConfig(dap,{ "typescript", "javascript" })
+
 -- setup adapter
 dap.adapters.node2 = nodejs.getNodeJsAdapter
 
